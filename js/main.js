5f9494047452c0509a580a5de0b0222e0edd41aa
Miguel Pardo <migsub77@gmail.com>
2017-02-05 15:13:03 -0800

    changed ids of html and set up coinCount notifications function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
+let total = 0;
+let currentCoin;
 let coin = [].slice.call(document.getElementsByClassName('coin'));
 const calcBtn = document.getElementById('calcBtn');
 const newCoinInput = document.getElementById('newCoinInput');
 const saveBtn = document.getElementById('saveBtn');
-let currentCoin;
+const userInput = document.getElementById('userInput');
+const coinCount = document.getElementsByClassName('coinCount');
+const coinValues = {};
 
+// Adds event handlers to coins and save button.
+// Values of coins will change after save button is pressed.
 function changeCoinValue() {
   for(var i = 0; i < 4; i++) {
     let currCoin = coin[i];
@@ -18,3 +24,22 @@ function changeCoinValue() {
 };
 
 changeCoinValue();
+
+// changes input that user enters
+function changeInput(newInput) {
+  userInput.value = newInput;
+}
+
+// Coin values array is passed in and finds labels for coin count notifications.
+function findCurrCoinCount(coinValues) {
+  for(var i = 0; i < 4; i++) {
+    let currCoinCount = coinCount[i];
+    currCoinCount.innerHTML = '';
+    for (var key in coinValues) {
+      // key must match current coin and must have a value in matching key.
+      if (key === coinCount[i] && coinCount[key]) {
+        currLabel.innerHTML = coinValues[key];
+      }
+    }
+  }
+}
